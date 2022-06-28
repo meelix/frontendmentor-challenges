579596c29e52bb841174f88793225099224a4bb0
Matthias Haid <matthias@villa-marianne.com>
2022-06-28 12:07:40 +0200

    update

--- expenses-chart-component-main/data.js
+++ expenses-chart-component-main/data.js
@@ -2,18 +2,14 @@ fetch('./data.json')
   .then(response => response.json())
   .then(data => {
     let heighest = 0;
-    let total = 0;
     data.forEach(dataSet => {
         document.querySelector(`[data-day="${dataSet.day}"] .info`).innerHTML = `$${dataSet.amount}`;
         document.querySelector(`[data-day="${dataSet.day}"] .bar`).style.setProperty("--percent", dataSet.amount + "%");
         if (!heighest || heighest < dataSet.amount) {
             heighest = dataSet.amount
         }
-        total += dataSet.amount;
     });
     document.documentElement.style.setProperty('--heighest', heighest);
-
-    document.querySelector(`.total`).innerHTML = `$${total}`;
 });
 
 const weekDays = ["sun","mon","tue","wed","thu","fri","sat"]
